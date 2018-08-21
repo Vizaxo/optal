481bdbde3e310faa0f083cc553781f3a0f039609
Vizaxo <vizaxo@mailfence.com>
2018-08-21 22:21:39 +0100

    Add simple lambda calculus Term type and some example terms

---
+++ src/Examples.hs
@@ -0,0 +1,19 @@
+module Examples where
+
+import Term
+
+-- | λ0
+identity :: Term
+identity = TLam (TVar 0)
+
+-- | λ0 0
+appSelf :: Term
+appSelf = TLam (TApp (TVar 0) (TVar 0))
+
+-- | λλ1
+fst' :: Term
+fst' = TLam (TLam (TVar 1))
+
+-- | λλ0
+snd' :: Term
+snd' = TLam (TLam (TVar 0))
---
+++ src/Term.hs
@@ -0,0 +1,11 @@
+module Term where
+
+-- | Lambda terms
+data Term
+  = TVar Index     -- ^ de Bruijn representation of variables (0-indexed)
+  | TLam Term      -- ^ λbody
+  | TApp Term Term -- ^ f x
+
+-- | The index used for de Bruijn indexing the terms
+type Index = Int
+
